9178475c686b6880c813811372c1a3c8c96e6db7
macgyver2008 <88918816+macgyver2008@users.noreply.github.com>
2021-10-16 13:19:22 +0900

    Update tic.py

--- tic.py
+++ tic.py
@@ -1,4 +1,41 @@
 import random
+end = 0
+count = 0
+def simpan(tic_map) :
+    if tic_map[0]== "o" and tic_map[1]== "o" and tic_map[3]== "o" :
+        print("computer win")
+    elif tic_map[3]== "o" and tic_map[4]== "o" and tic_map[5] == "o" :
+        print("computer win")                                        # elif tic_map[4] == "o" and tic_map[5] == "o" and tic_map[6] == "o" :
+    elif tic_map[6]== "o" and tic_map[7]== "o" and tic_map[8] == "o" :
+        print("computer win")
+    elif tic_map[0]== "o" and tic_map[3]== "o" and tic_map[6] == "o":
+        print("computer win")
+    elif tic_map[1]== "o" and tic_map[7]== "o" and tic_map[3] == "o":
+        print("computer win")                                             # elif tic_map[4] == "o" and tic_map[5] == "o" and tic_map[6] == "o" :
+    elif tic_map[2]== "o" and tic_map[5]== "o" and tic_map[8] == "o":
+        print("computer win")
+    elif tic_map[0]== "o" and tic_map[4]== "o" and tic_map[8] == "o":
+        print("computer win")
+    elif tic_map[2]== "o" and tic_map[4]== "o" and tic_map[6] == "o":
+        print("computer win")
+
+    if tic_map[0]== "x" and tic_map[1]== "x" and tic_map[3] == "x":
+        print("You win")
+    elif tic_map[3]== "x" and tic_map[4]== "x" and tic_map[5] == "x":
+        print("You win")  # elif tic_map== "x"[4] == "o" and tic_map[5] == "o" and tic_map[6] == "o" :
+    elif tic_map[6]== "x" and tic_map[7]== "x" and tic_map[8] == "x":
+        print("You win")
+    elif tic_map[0]== "x" and tic_map[3]== "x" and tic_map[6] == "x":
+        print("You win")
+    elif tic_map[1]== "x" and tic_map[7]== "x" and tic_map[3] == "x":
+        print("You win")  # elif tic_map[4] == "o" and tic_map[5] == "o" and tic_map[6] == "o" :
+    elif tic_map[2]== "x" and tic_map[5]== "x" and tic_map[8] == "x":
+        print("You win")
+    elif tic_map[0]== "x" and tic_map[4]== "x" and tic_map[8] == "x":
+        print("You win")
+    elif tic_map[2]== "x" and tic_map[4]== "x" and tic_map[6] == "x":
+        print("You win")
+
 def make_map() :
     tic_map = ["" for i in range(9)]            #tic_map = []
     return tic_map                              # for i in range(9):
@@ -24,7 +61,7 @@ def print_map(tic_map):
     print(" | " + tic_map[3] + " | " + tic_map[4] + " | " + tic_map[5] + " | ")
     print(" | " + tic_map[6] + " | " + tic_map[7] + " | " + tic_map[8] + " | ")
 
-tic_map = make_map()                      #    tic_map.append(0)
+tic_map = make_map()
 print('player_x')
 print("AI_o")
 while True:
@@ -35,3 +72,11 @@ while True:
     tic_map = AI(tic_map)
 
     print_map(tic_map)
+    simpan(tic_map)
+    for i in range(9):
+        if not tic_map[i] == "":
+            count += 1
+    if count == 9:
+        print('tie')
+    else:
+        count = 0
