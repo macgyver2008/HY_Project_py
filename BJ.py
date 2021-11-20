78bb307f778e190804b7016245a118e06604aad1
macgyver2008 <88918816+macgyver2008@users.noreply.github.com>
2021-11-20 13:21:32 +0900

    Create BJ.py

---
+++ BJ.py
@@ -0,0 +1,22 @@
+#2
+# c = int(input())
+# d = list(map(int, input().split(' ')))
+# count = 0
+# for i in range(len(d)) :
+#     a = d[i]
+#     fl = 1
+#     for r in range(1, a//2 + 1):
+#         if a%r ==0 :
+#             fl += 1
+#             if fl >= 3:
+#                 break
+#     if fl ==2 :
+#         count += 1
+# print(count)
+
+
+#3
+# c=int(input())
+# d=list(map(int,input().split(' ')))
+# d.sort()
+# print(d[0],d[c-1])
